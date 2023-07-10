d6c14d80ea953990f6dabfa2a86f28400fb09910
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-07-10 15:47:06 +0200

    without stripe

--- app/models/produit.rb
+++ app/models/produit.rb
@@ -5,11 +5,11 @@ class Produit < ApplicationRecord
         name
     end
 
-    after_create do
-        product = Stripe::Product.create(name: name)
-        price = Stripe::Price.create(product: product, unit_amount: self.price, currency: 'eur')
-        update(stripe_product_id: product.id, stripe_price_id: price.id)
-    end
+   # after_create do
+   #     product = Stripe::Product.create(name: name)
+   #     price = Stripe::Price.create(product: product, unit_amount: self.price, currency: 'eur')
+   #     update(stripe_product_id: product.id, stripe_price_id: price.id)
+   # end
 
 
 end
