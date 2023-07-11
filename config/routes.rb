fb36929d3be4ae1b99b118431f2b6a2ab5c6010b
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-07-11 17:31:29 +0200

    add to cart et cart affichédans appli layout

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
   get "success", to: "checkout#success"
   get "cancel", to: "checkout#cancel"
 
-  
   resources :webhooks, only: [:create]
   get "success", to: "checkout#success"
   get "cancel", to: "checkout#cancel"
 
   root "produits#index"
 
- #root "home#index"
+  post "produits/add_to_cart/:id", to: "produits#add_to_cart", as: "add_to_cart"
+  delete "produits/remove_from_cart/:id", to: "produits#remove_from_cart", as: "remove_from_cart"
+
+
 end
