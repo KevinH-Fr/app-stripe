b2de5153a13d2827f40df363567b3463c0dbb798
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-07-11 17:14:39 +0200

    deplacement logique dans controller checkout pour success page

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -13,11 +13,18 @@ class CheckoutController < ApplicationController
       cancel_url: cancel_url,   
       })
 
-     redirect_to session.url, allow_other_host: true, status: 303
+    redirect_to session.url, allow_other_host: true, status: 303
 
   end
 
   def success
+    @session_with_expand = Stripe::Checkout::Session
+      .retrieve({ id: params[:session_id], expand: ["line_items"] }) 
+
+    @session_with_expand.line_items.data.each do |line_item|
+      @product_id = Produit.find_by(stripe_product_id: line_item.price.product)
+    end
+
   end
 
   def cancel
