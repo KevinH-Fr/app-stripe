dfe02b00fca43f439071526c3a0a05db08552693
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-07-12 13:26:05 +0200

    edit on produit name and prices buy and sub if changed ok

--- app/models/produit.rb
+++ app/models/produit.rb
@@ -23,30 +23,28 @@ class Produit < ApplicationRecord
       
         update(stripe_product_id: product.id, stripe_price_id: one_time_price.id, stripe_subscription_price_id: subscription_price.id)
       end
-      
-      
 
-    #after_update do
+    after_update do
+        product = Stripe::Product.update(stripe_product_id, {name: name})
+    end
 
-    #   product = Stripe::Product.update(stripe_product_id, { name: self.name })
-        # Créer un nouveau tarif avec le nouveau montant
-    #    new_price = Stripe::Price.create(
-    #        product: stripe_product_id,
-    #        unit_amount: self.price,
-    #        currency: 'eur'
-    #    )
-    
-        # Mettre à jour le produit avec le nouvel identifiant de tarif
-    #    Stripe::Product.update(stripe_product_id, {
-    #        metadata: { stripe_price_id: new_price.id }
-    #    })
-    
-        # Désactiver l'ancien tarif
-    #    Stripe::Price.update(stripe_price_id, active: false) if stripe_price_id.present?
-    
-        # Update the product's stripe_price_id with the new price ID
-    #    update(stripe_price_id: new_price.id)
+    after_update  :create_and_assign_new_stripe_price, if: :saved_change_to_price?
 
-    #end
-      
+    def create_and_assign_new_stripe_price 
+        one_time_price = Stripe::Price.create(
+            product: self.stripe_product_id, 
+            unit_amount: self.price, 
+            currency: 'eur')
+
+        subscription_price = Stripe::Price.create(
+            product: self.stripe_product_id, 
+            recurring: {
+                interval: 'month',
+            },
+            unit_amount: self.price/12, # Set the subscription price amount here
+            currency: 'eur')
+
+        update(stripe_price_id: one_time_price.id, stripe_subscription_price_id: subscription_price.id )
+    end
+    
 end
